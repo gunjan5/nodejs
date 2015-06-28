bbb9a9b40476de6d0be881f29c8a9d8af49a2ecc
Gunjan Patel <patelgunjan5@gmail.com>
2015-06-27 17:29:13 -0700

    oops

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 var http = require ('http');
 var myServer = http.createServer( function(request, response) {
-  response.writeHead(200, {"Content-Type": "test/plain"});
+  response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello");
   response.end();
 });
