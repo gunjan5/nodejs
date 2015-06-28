3713765b2961414c8c9d08e0f4fa25e99b7f2f9b
Gunjan Patel <patelgunjan5@gmail.com>
2015-06-28 03:35:06 -0700

    moar routes vars

--- express/index.js
+++ express/index.js
@@ -10,6 +10,12 @@ app.get('/me', function(req, res) {
   res.send('@Gunjan_Patel1 <br> Github: gunjan5');
 });
 
+app.get('/who/:name?/:title?', function(req, res) {
+  var name = req.params.name;
+  var title = req.params.title;
+  res.send('<h2>' + name + '</h2> the <h2> <em>' + title + '</em> </h2> was here!!!');
+});
+
 var server = app.listen(4000, function() {
   console.log('listening on port 4000 on localhost');
 });
